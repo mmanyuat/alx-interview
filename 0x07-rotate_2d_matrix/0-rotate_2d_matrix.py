3118da5dc71734398a665209e0b959a12a107835
mmanyuat <mmanyuat201@gmail.com>
2024-11-21 16:53:31 +0000

    differenced

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Rotate a 2D matrix 90 degrees clockwise in-place.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates a 2D matrix 90 degrees clockwise in-place.
+    Args:
+        matrix (list of list of int): The n x n 2D matrix to rotate.
+    """
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n):
+        matrix[i].reverse()
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,7 @@
+Matrix manipulation is a fundamental topic in programming and computer science.
+ This project helps strengthen problem-solving skills by focusing on:
+
+Transposing matrices.
+Reversing matrix rows.
+Implementing in-place operations to optimize space complexity.
+The solution avoids creating additional data structures, demonstrating an efficient use of memory.
